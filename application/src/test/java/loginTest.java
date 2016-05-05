bc95eb41bf460b75aabf603a68cc37d9c0a7d8de
yers <yerskatmw@gmail.com>
2016-05-05 10:16:28 +0800

    junit

---
+++ application/src/test/java/loginTest.java
@@ -0,0 +1,25 @@
+package application;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class loginTest extends TestCase 
+{
+	@Test
+	public void testAdd() 
+	{
+		login testLogin = new login();
+		
+		//check for equality
+		assertEquals(testLogin.checkUser("xxxxx","xxxx") , "");
+		//assertEquals(testLogin.checkUser("1","1") , "9");
+		//assertEquals(testLogin.checkUser("1","1") , "");
+		
+		//check for false condition
+		//assertFalse(num > 6);
+		
+		//check for not null value
+		//assertNotNull(str);
+	}
+}
